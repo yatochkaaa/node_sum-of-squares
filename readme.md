c70e1c2f7aeefcd80c359f36added9e40f96f208
mgrinko <glukym@gmail.com>
2021-10-18 17:51:00 +0300

    prepare 1st version

--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 # NodeJS - Sum of squares
-Create 3 modules:
-- `function sum(x, y)`
-- `function square(x)`
-- `function sumOfSquares(x, y)` using `sum` and `square`
+- Create 3 CommonJS modules:
+  - `function sum(x, y)`
+  - `function square(x)`
+  - `function sumOfSquares(x, y)` using `sum` and `square`
 
 Each module will be tested separatelly
--- src/sum.test.js
+++ src/sum.test.js
@@ -1,7 +1,10 @@
 'use strict';
+
 /* global require, describe, test, expect */
 
-const sum = require('./sum');
+const { square } = require('./square');
+const { sum } = require('./sum');
+const { sumOfSquares } = require('./sumOfSquares');
 
 describe('sum', () => {
   test('should return a sum of positive numbers', () => {
@@ -12,5 +15,39 @@ describe('sum', () => {
   test('should return a sum of negative numbers', () => {
     expect(sum(-1, -2))
       .toBe(-3);
+  });
+});
+
+describe('square', () => {
+  test('should return a square of a positive number', () => {
+    expect(square(6))
+      .toBe(36);
+  });
+
+  test('should return a square of a negative number', () => {
+    expect(square(-7))
+      .toBe(49);
+  });
+
+  test('should return a square of 0', () => {
+    expect(square(0))
+      .toBe(0);
+  });
+});
+
+describe('sumOfSquare', () => {
+  test('should return a sum of squares for positive numbers', () => {
+    expect(sumOfSquares(2, 3))
+      .toBe(13);
+  });
+
+  test('should return a sum of squares for negative numbers', () => {
+    expect(sumOfSquares(-7, -5))
+      .toBe(74);
+  });
+
+  test('should return a sum of squares for 0 and any other number', () => {
+    expect(sumOfSquares(0, -1))
+      .toBe(1);
   });
 });
